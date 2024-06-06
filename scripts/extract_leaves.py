e992af6edf892c43d595b9cd3d06e5b4cc5f48ad
tubks <radola.j@gmail.com>
2024-06-06 17:08:13 +0200

    run from cmd version

--- scripts/extract_leaves.py
+++ scripts/extract_leaves.py
@@ -1,15 +1,5 @@
 import re
-import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
-from unidecode import unidecode
-
-alignment_xml_path = 'dat\ChatBotte_MasterCat.ali.xml'
-
-path_eng_sents = "scripts/sents_ChatBotte_eng.txt"
-path_fr_sents = "scripts/sents_ChatBotte_fr.txt"
-
-with open(alignment_xml_path, 'r') as filehandle:
-    soup = BeautifulSoup(filehandle, 'xml')
 
 
 def extract_leaves_strings_only(soup):
@@ -29,7 +19,7 @@ def extract_leaves_strings_only(soup):
     return all_leaf_alignments_strings_only
 
 
-def extract_leaves_with_spans(soup, separate_by_sentence=False):
+def extract_leaves_with_span_info(soup, separate_by_sentence=True):
     # returns a dict with linkIDs as keys, lists of 2 tuples containing info about the spans that are linked (beginPos, endPos, string) as values
     # the first linkList contains linkGroups with all the alignments (linkList level='chunk')
     linkGroups = soup.linkList.find_all('linkGroup')
@@ -53,39 +43,48 @@ def extract_leaves_with_spans(soup, separate_by_sentence=False):
     return all_leaves
 
 
-dict_all_spans_by_sentence, dict_all_spans = extract_leaves_with_spans(
-    soup, separate_by_sentence=True)
-all_leaves_strings = extract_leaves_strings_only(soup)
-
-alignment_filename = 'chat_alignments.txt'
-
-
-def convert_to_pharaoh(dict_all_spans_by_sentence, alignment_filename):
-    # extracting (one or more) digits preceded by underscore, separated by a dash
+def convert_to_pharaoh(dict_all_spans_by_sentence, alignment_filepath):
+    # regex matching (one or more) digits preceded by underscore, separated by a dash
     pattern = r"_(\d+-\d+)"
     for i, sentence_spans in enumerate(dict_all_spans_by_sentence):
-        with open(alignment_filename, 'a+') as file:
+        with open(alignment_filepath, 'a+') as file:
             file.write(f'{i}\t')
             for span_id in sentence_spans.keys():
                 fr_span, eng_span = re.findall(pattern, span_id)
                 fr_begin, fr_end = fr_span.split('-')
                 eng_begin, eng_end = eng_span.split('-')
-                if fr_begin == fr_end and eng_begin == eng_end:  # 1 fr to 1 eng, sure alignment
+                if fr_begin == fr_end and eng_begin == eng_end:  # 1 fr to 1 eng token, sure alignment
                     file.write(f'{fr_begin}-{eng_begin} ')
                 elif fr_begin != fr_end and eng_begin == eng_end:
-                    for j in range(int(fr_begin), int(fr_end)+1):  # many fr to 1 eng
+                    # many fr to 1 eng, potential
+                    for j in range(int(fr_begin), int(fr_end)+1):
                         file.write(f'{j}p{eng_begin} ')
                 elif fr_begin == fr_end and eng_begin != eng_end:
-                    for j in range(int(eng_begin), int(eng_end)+1):  # 1 fr to many eng
+                    # 1 fr to many eng, potential
+                    for j in range(int(eng_begin), int(eng_end)+1):
                         file.write(f'{fr_begin}p{j} ')
-                elif fr_begin != fr_end and eng_begin != eng_end:  # many fr to many eng
+                elif fr_begin != fr_end and eng_begin != eng_end:  # many fr to many eng, potential
                     for j in range(int(fr_begin), int(fr_end)+1):
                         for k in range(int(eng_begin), int(eng_end)+1):
                             file.write(f'{j}p{k} ')
             file.write('\n')
 
 
-convert_to_pharaoh(dict_all_spans_by_sentence, alignment_filename)
+if __name__ == "__main__":
+    alignment_xml_path = 'dat/ChatBotte_MasterCat.ali.xml'
+    alignment_xml_path = input("path to the xml alignments file: ")
+
+    with open(alignment_xml_path, 'r') as filehandle:
+        soup = BeautifulSoup(filehandle, 'xml')
+
+    dict_all_spans_by_sentence, dict_all_spans = extract_leaves_with_span_info(
+        soup, separate_by_sentence=True)
+    all_leaves_strings = extract_leaves_strings_only(soup)
+
+    alignment_filepath = 'chat_alignments.txt'
+    alignment_filepath = input(
+        "path to file where you want to save the w2w leaf alignments: ")
+    convert_to_pharaoh(dict_all_spans_by_sentence, alignment_filepath)
 
 
 # def build_indices(path_eng_sents, path_fr_sents):
--- scripts/extract_sentences.py
+++ scripts/extract_sentences.py
@@ -1,34 +1,19 @@
-import xml.etree.ElementTree as ET
-
-alignment_xml_path = 'dat\ChatBotte_MasterCat.ali.xml'
-tree = ET.parse(alignment_xml_path)
-root = tree.getroot()
-print(type(tree), type(root))
-print(f"Root tag: {root.tag}")
-for child in root:
-    print(f"Children tags with attrib:{child.tag}, {child.attrib}")
-link_list_level_chunk = root[1]
-link_list_level_token = root[2]
-print(link_list_level_chunk.attrib, link_list_level_token.attrib)
+from bs4 import BeautifulSoup
+import sys
 
 
-print(root.findall(
-    "./linkList/linkGroup/link[@parentID'='ROOT']/"))
-
-
-def extract_sentences(link_list_level_chunk):
+def extract_sentences(soup):
+    # the first linkList contains linkGroups with all the alignments (linkList level='chunk')
+    linkGroups = soup.linkList.find_all('linkGroup')
     sentence_tuples = []
-    for link_group in link_list_level_chunk:
-        for link in link_group:
-            if link.get('parentID') == "ROOT":
-                sentence_tuples.append((link[0].text, link[1].text))
+    for linkGroup in linkGroups:
+        links = linkGroup.find_all('link')
+        for link in links:
+            if link['parentID'] == "ROOT":
+                spans = link.find_all('docSpan')
+                sentence_tuples.append(
+                    (spans[0].string, spans[1].string))
     return sentence_tuples
-
-
-sents = extract_sentences(link_list_level_chunk)
-
-path_eng = "scripts/sents_ChatBotte_eng.txt"
-path_fr = "scripts/sents_ChatBotte_fr.txt"
 
 
 def write_eng_fr_to_files(sentences_tuple, path_eng_file, path_fr_file):
@@ -38,4 +23,24 @@ def write_eng_fr_to_files(sentences_tuple, path_eng_file, path_fr_file):
             eng_file.write(f'{i}\t{sent_pair[1]}\n')
 
 
-write_eng_fr_to_files(sents, path_eng, path_fr)
+if __name__ == "__main__":
+
+    alignment_xml_path = 'dat/ChatBotte_MasterCat.ali.xml'
+
+    alignment_xml_path = input("path to the xml alignments file: ")
+
+    with open(alignment_xml_path, 'r') as filehandle:
+        soup = BeautifulSoup(filehandle, 'xml')
+
+    sents = extract_sentences(soup)
+
+    path_eng = "scripts/sents_ChatBotte_eng.txt"
+    path_fr = "scripts/sents_ChatBotte_fr.txt"
+
+    path_eng = input(
+        "path to file where you want to save the eng sentences: ")
+    path_fr = input("path to file where you want to save the fr sentences: ")
+    # path_eng = sys.argv[2]
+    # path_fr = sys.argv[3]
+
+    write_eng_fr_to_files(sents, path_eng, path_fr)
