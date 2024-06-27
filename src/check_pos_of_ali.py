2ed6af9128659502e02023e5718593cffa749c60
tubks <radola.j@gmail.com>
2024-06-27 17:14:25 +0200

    retrieving pos tags of alignments and tokens, pos tags distributions

---
+++ src/check_pos_of_ali.py
@@ -0,0 +1,34 @@
+import re
+import pandas as pd
+from collections import Counter
+fr_pos_list = ['pos_tags/fr_pos_A.txt','pos_tags/fr_pos_B.txt','pos_tags/fr_pos_C.txt','pos_tags/fr_pos_D.txt','pos_tags/fr_pos_V.txt']
+eng_pos_list = ['pos_tags/eng_pos_A.txt','pos_tags/eng_pos_B.txt','pos_tags/eng_pos_C.txt','pos_tags/eng_pos_D.txt','pos_tags/eng_pos_V.txt']
+ali_w2w_list = ['ali/LAuberge_TheInn/ali_w2w_LAuberge_TheInn.txt','ali/LaBarbeBleue_BlueBeard/ali_w2w_LaBarbeBleue_BlueBeard.txt','ali/ChatBotte_MasterCat/ali_w2w_ChatBotte_MasterCat.txt','ali/LaDerniereClasse_TheLastLesson/ali_w2w_LaDerniereClasse_TheLastLesson.txt','ali/LaVision_TheVision/ali_w2w_LaVision_TheVision.txt']
+result_pos_list = ['pos_tags/pos_A.csv','pos_tags/pos_B.csv','pos_tags/pos_C.csv','pos_tags/pos_D.csv','pos_tags/pos_V.csv']
+all_fr_pos_list=[]
+all_eng_pos_list=[]
+pos_frequencies=[]
+for fr_pos, eng_pos, ali_w2w, result_pos in zip(fr_pos_list,eng_pos_list,ali_w2w_list,result_pos_list):
+    ali_pos_list=[]
+    with open(eng_pos, 'r') as eng_file, open(fr_pos, 'r') as fr_file, open(ali_w2w, 'r') as ali_file:
+        # , open(result_pos, 'a+') as chat_resolu # add above if we want to save the pos alignments to a file
+        for line_eng, line_fr, line_ali in zip(eng_file, fr_file, ali_file):
+            pos_fr_list = line_fr.split('\t')[1].split()
+            pos_eng_list = line_eng.split('\t')[1].split()
+            ali_list = line_ali.split('\t')[1].split() # takes the alignments without the id\t, makes a list of alignments
+            # print(pos_fr_list,pos_eng_list,ali_list)
+            for ali in ali_list:
+                fr_id,eng_id = re.split('-|p', ali)
+                # chat_resolu.write(f'{pos_fr_list[int(fr_id)]}-{pos_eng_list[int(eng_id)]} ')
+                try:
+                    ali_pos_list.append(f'{pos_fr_list[int(fr_id)]}-{pos_eng_list[int(eng_id)]}')
+                except:
+                    print(fr_id, eng_id, pos_fr_list,pos_eng_list)
+            all_fr_pos_list.extend(pos_fr_list)
+            all_eng_pos_list.extend(pos_eng_list)
+    df = pd.DataFrame(ali_pos_list, columns=['alignment_pos'])
+    print(df.value_counts())
+    c_fr = Counter(all_fr_pos_list)
+    c_eng = Counter(all_eng_pos_list)
+    print(c_eng, c_fr)
+    df.value_counts().to_csv(result_pos)
---
+++ src/get_pos_tags_tokens.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+import re
+
+def get_pos_tag_tokens(soup,path_eng_file,path_fr_file):
+    # creates two files specified in path_eng_file and path_fr_file which contain the information about the pos of every token
+    # the second linkList contains linkGroups with all the annotations (linkList level='token')
+    linkLists = soup.find_all('linkList')
+    linkList = linkLists[1]
+    linkGroups=linkList.find_all('linkGroup')
+    # print(linkGroups, len(linkGroups))
+    with open(path_eng_file, 'a+') as eng_file, open(path_fr_file, 'a+') as fr_file:
+        for i,linkGroup in enumerate(linkGroups):
+            annotations_fr = linkGroup.find_all('annotation', id=re.compile("doc_fr annot_token_"))
+            annotations_eng = linkGroup.find_all('annotation', id=re.compile("doc_en annot_token_"))
+            if annotations_fr:
+                fr_file.write(f"{i}\t")
+                for annotation in annotations_fr:
+                    pos = annotation.find_all('mark',cat='POS')[0].string
+                    fr_file.write(f'{pos} ')
+                fr_file.write("\n") 
+            if annotations_eng:
+                eng_file.write(f"{i}\t")
+                for annotation in annotations_eng:
+                    pos = annotation.find_all('mark',cat='POS')[0].string
+                    eng_file.write(f'{pos} ')
+                eng_file.write("\n") 
+
+
+if __name__ == "__main__":
+    # alignment_xml_path = input("path to the xml alignments file: ")
+    alignment_xml_path = 'dat/LaVision_TheVision.ali.xml'
+    eng_path="eng_pos_V.txt"
+    fr_path="fr_pos_V.txt"
+    with open(alignment_xml_path, 'r') as filehandle:
+        soup = BeautifulSoup(filehandle, 'xml')
+    get_pos_tag_tokens(soup,eng_path, fr_path)
+    #     soup, separate_by_sentence=True)
+    # dict_all_leaves_by_sentence, dict_all_leaves = extract_leaves_with_span_info(
+    #     soup, separate_by_sentence=True)
+    # print(dict_all_leaves_by_sentence, dict_all_leaves)
